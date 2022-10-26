61cb0717ceae0cc3826cc0cbc8c76c61dff025b7
Jelle Baele <jellebaele@hotmail.com>
2022-10-26 18:23:21 +0200

    Install mongoose and create User Model

---
+++ src/model/User.ts
@@ -0,0 +1,27 @@
+import mongoose, { Document, ObjectId } from 'mongoose';
+
+export interface IUser {
+  username: string;
+  password: string;
+  // Could also be a row of ROLES (enum)
+  isAdmin: boolean;
+}
+
+export interface IUserDocument extends IUser, Document {
+  _id: ObjectId;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const UserSchema = new mongoose.Schema(
+  {
+    username: { Type: String, required: true, unique: true },
+    password: { Type: String, required: true },
+    isAdmin: { type: Boolean, required: false, default: false },
+  },
+  { timestamps: true }
+);
+
+const UserModel = mongoose.model<IUserDocument>('User', UserSchema);
+
+export default UserModel;
