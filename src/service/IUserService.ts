86807604e95e1d4c516bb832b5bb9fb7cd77fea4
Jelle Baele <jellebaele@hotmail.com>
2022-10-28 19:04:04 +0200

    Extend userservice

--- src/service/IUserService.ts
+++ src/service/IUserService.ts
@@ -1,14 +1,16 @@
-import { FilterQuery, QueryOptions } from 'mongoose';
+import { FilterQuery, ObjectId, QueryOptions } from 'mongoose';
 import { IUser, IUserDocument } from '../model/User';
 
 export interface IUserService {
   createUser(user: IUser): Promise<IUserDocument | Error>;
   getAllUsers(limit?: number): Promise<(IUserDocument | null)[]>;
   getUserById(id: string): Promise<IUserDocument | null>;
+  getUserByUsername(username: string): Promise<IUserDocument | null>;
   updateUserById(
     id: string,
     query: FilterQuery<IUserDocument>,
     options?: QueryOptions
   ): Promise<IUserDocument | null>;
   deleteUserById(id: string): Promise<boolean | null>;
+  validatePassword(plainTextPassword: string, userId: string): Promise<boolean>;
 }
