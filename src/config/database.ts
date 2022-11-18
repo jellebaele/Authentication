78ba4509e831fe4641386ebaed70eba4a7dffff4
Jelle Baele <jellebaele@hotmail.com>
2022-11-18 16:41:47 +0100

    Update dev script + adjust mongo connection

---
+++ src/config/database.ts
@@ -0,0 +1,3 @@
+export const {
+  MONGO_CONNECTION_URL = 'mongodb://localhost:27017/AuthenticationExample',
+} = process.env;
---
+++ src/utils/loaders/database.ts
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose';
+import { MONGO_CONNECTION_URL } from '../../config';
+
+function setupMongoose(): void {
+  mongoose.connect(MONGO_CONNECTION_URL, (err) => {
+    if (err) throw err;
+    console.log(`Connected to mongoDB at ${MONGO_CONNECTION_URL}`);
+  });
+}
+
+export default setupMongoose;
