a58693aac75d412dab65b7745b609c0bda69c18e
Jelle Baele <jellebaele@hotmail.com>
2022-11-19 15:25:50 +0100

    Make user routes protected

--- src/api/routes/v1.ts
+++ src/api/routes/v1.ts
@@ -1,11 +1,10 @@
 import { Router } from 'express';
-import { asyncErrorHandler } from '../middleware';
 import authRouter from './authRoute';
 import userRouter from './userRoute';
 
 const v1Router: Router = Router();
 
-v1Router.use('/users', asyncErrorHandler(userRouter));
+v1Router.use('/users', userRouter);
 v1Router.use('/auth', authRouter);
 
 export default v1Router;
