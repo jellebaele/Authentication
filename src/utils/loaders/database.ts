b09154a31808206430ee9863ab2ce1dd5415bd78
Jelle Baele <jellebaele@hotmail.com>
2022-11-20 20:48:21 +0100

    Added docker-compose for cache and database

--- src/utils/loaders/database.ts
+++ src/utils/loaders/database.ts
@@ -4,7 +4,7 @@ import { MONGO_CONNECTION_URL } from '../../config';
 function setupMongoose(): void {
   mongoose.connect(MONGO_CONNECTION_URL, (err) => {
     if (err) throw err;
-    console.log(`Connected to mongoDB at ${MONGO_CONNECTION_URL}`);
+    console.log('Connected to mongoDB');
   });
 }
 
