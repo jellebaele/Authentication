b355aef3684f25491b386f25fe48f94057d804e5
Jelle Baele <jellebaele@hotmail.com>
2022-11-25 17:01:56 +0100

    load env variables at the very beginning

--- src/utils/loaders/index.ts
+++ src/utils/loaders/index.ts
@@ -1,12 +1,10 @@
 import setupExpress from './express';
 import { Express } from 'express';
-import dotenv from 'dotenv';
 import setupMongoose from './database';
 import setupCacheStore from './cache';
 
 const setupServer = (): Express | undefined => {
   try {
-    dotenv.config();
     setupMongoose();
     const cacheStore = setupCacheStore();
     const app = setupExpress(cacheStore);
